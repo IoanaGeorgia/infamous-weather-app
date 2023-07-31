b3754ef142ed58761baa2da7bc1ad5db2a13a84c
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-07-31 17:36:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# infamous-weather-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-3aaux4)
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='mainWrapper'>
+
+  <div class='overview'>
+
+  <div class='overviewHeader'>
+  <button class='overviewSearchButton'> Search for places </button>
+  <button
+            class="material-icons-outlined overviewLocationButton"
+            style="font-size: 21px"
+          >
+            my_location
+          </button>
+  </div>
+
+  </div>
+
+
+
+  <div class='details'>
+
+  </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  data(){
+    return{
+      weatherObj:{}
+    }
+  },
+  mounted(){
+    this.getWeather()
+  },
+  methods:{
+        getWeather() {
+      fetch('http://api.weatherapi.com/v1/forecast.json?key=51b610f3b328481a908142828233107&q=London&days=3&aqi=no&alerts=no')
+        .then((response) => response.json())
+        .then((response) => {this.weatherObj = response; console.log(response)});
+      
+      console.log(this.weatherObj)
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
