cdc5fc5d546e7a80bb1f8efd56ea8f17d4c706db
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-07-31 18:18:32 +0300

    overview styles schematically put in place, call to the api written

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -13,6 +13,11 @@
           </button>
   </div>
 
+<!-- <img src='https://cdn.weatherapi.com/weather/64x64/day/296.png'> -->
+
+<!-- <span>{{"https"+weatherToday.icon}}</span> -->
+<img src={{getIcon(weatherToday.icon)}}>
+<span>{{weatherToday.text}}</span>
   </div>
 
 
@@ -32,19 +37,48 @@ export default {
   name: 'LandingPage',
   data(){
     return{
-      weatherObj:{}
+      weatherObj:{},
+      weatherToday:{
+          icon:'',
+          text:'',
+          humidity:'',
+          wind_mph:'',
+          wind_dir:'',
+          vis_miles:'',
+          pressure_in:'',
+          temp_c:'',
+          temp_f:'',
+      }
     }
   },
   mounted(){
+    console.log('brr')
     this.getWeather()
   },
   methods:{
-        getWeather() {
-      fetch('http://api.weatherapi.com/v1/forecast.json?key=51b610f3b328481a908142828233107&q=London&days=3&aqi=no&alerts=no')
-        .then((response) => response.json())
-        .then((response) => {this.weatherObj = response; console.log(response)});
-      
-      console.log(this.weatherObj)
+    getIcon(a){
+      console.log("https:"+a)
+      return "https:"+a
+    },
+       async getWeather() {
+
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=51b610f3b328481a908142828233107&q=London&days=3&aqi=no&alerts=no')
+    const json =  await response.json();
+    this.weatherObj = json
+
+    this.weatherToday={
+          icon:json.current.condition.icon,
+          text:json.current.condition.text,
+          humidity:json.current.humidify,
+          wind_mph:json.current.wind_mph,
+          wind_dir:json.current.wind_dir,
+          vis_miles:json.current.vis_miles,
+          pressure_in:json.current.pressure_in,
+          temp_c:json.current.temp_c,
+          temp_f:json.current.temp_f,
+      }
+
+    console.log(this.weatherObj, 'm')
     },
   }
 }
